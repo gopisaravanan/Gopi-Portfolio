b1b1af1055d23d0877106c872d3552046f151644
Gopi saravanan R <gopi.saravanan@kaaylabs.com>
2024-07-15 12:00:18 +0530

    resume updated

--- src/data/constants.js
+++ src/data/constants.js
@@ -11,7 +11,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Gopi-saravanan",
   resume:
-    "https://drive.google.com/file/d/1f6UkUtorKq5zLkM4qK9cFShThSE2-brJ/view?usp=drive_link",
+    "https://drive.google.com/file/d/1xR2WEhGjk7M5T7K4SfzJ5ppzi_858aUc/view?usp=drive_link",
   linkedin: "https://www.linkedin.com/in/gopi-saravanan-b812b7249",
 };
 
